93fd89dabab2b241e8a6b565928c1521473c49b2
Aakriti Sharma <aakritisharma627@gmail.com>
2026-02-21 16:34:02 +0530

    Initial commit

---
+++ src/main/java/com/moveinsync/metrobooking/MetrobookingApplication.java
@@ -0,0 +1,13 @@
+package com.moveinsync.metrobooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrobookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrobookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moveinsync/metrobooking/MetrobookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveinsync.metrobooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrobookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
